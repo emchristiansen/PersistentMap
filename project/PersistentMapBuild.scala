924532c0d3aea6b90772046cc039d56b907f2c99
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-09-30 15:32:14 -0700

    publish settings

--- project/PersistentMapBuild.scala
+++ project/PersistentMapBuild.scala
@@ -5,17 +5,47 @@ object PersistentMapBuild extends Build {
   def extraResolvers = Seq(
     resolvers ++= Seq(
       "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
-      "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-      "Local Maven Repository" at Path.userHome.asFile.toURI.toURL + "/.m2/repository",
-      Resolver.url("emchristiansen-scalatest-extra", url("https://raw.github.com/emchristiansen/scalatest-extra/master/releases"))( Patterns("[organisation]/[module]/[revision]/[artifact]-[revision].[ext]") )
-    )
-  )
+      "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"))
 
- val publishSettings = Seq(
-    organization := "emchristiansen",
-    publishMavenStyle := false,
-    publishTo := Some(Resolver.file("file", new File("./releases"))),
-    version := "0.1-SNAPSHOT")
+  val projectName = "PersistentMap"
+
+  val publishSettings = Seq(
+    name := projectName,
+
+    version := "0.1-SNAPSHOT",
+
+    organization := "st.sparse",
+
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (version.value.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+
+    publishMavenStyle := true,
+
+    publishArtifact in Test := false,
+
+    pomIncludeRepository := { _ => false },
+
+    licenses := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
+
+    homepage := Some(url("https://github.com/emchristiansen/PersistentMap")),
+
+    pomExtra := (
+      <scm>
+        <url>git@github.com:emchristiansne/PersistentMap.git</url>
+        <connection>scm:git:git@github.com:emchristiansen/PersistentMap.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>emchristiansen</id>
+          <name>Eric Christiansen</name>
+          <url>http://sparse.st</url>
+        </developer>
+      </developers>))
 
   val scalaVersionString = "2.10.3-RC3"
 
@@ -23,16 +53,13 @@ object PersistentMapBuild extends Build {
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-reflect" % scalaVersionString,
       "org.scala-lang" % "scala-compiler" % scalaVersionString,
-      "commons-io" % "commons-io" % "2.4",
       "com.typesafe.slick" %% "slick" % "1.0.1",
       "org.scala-lang" %% "scala-pickling" % "0.8.0-SNAPSHOT",
       "org.xerial" % "sqlite-jdbc" % "3.7.2" % "test",
       "org.slf4j" % "slf4j-nop" % "1.6.4" % "test",
       "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
       "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
-      "junit" % "junit" % "4.11" % "test"
-    )
-  )
+      "junit" % "junit" % "4.11" % "test"))
 
   def updateOnDependencyChange = Seq(
     watchSources <++= (managedClasspath in Test) map { cp => cp.files })
@@ -47,21 +74,18 @@ object PersistentMapBuild extends Build {
       "-language:implicitConversions",
       "-language:existentials",
       // "-language:reflectiveCalls",
-      "-language:postfixOps"
-    )
-  )
+      "-language:postfixOps"))
 
   def moreSettings =
     Project.defaultSettings ++
-    extraResolvers ++
-    extraLibraryDependencies ++
-    scalaSettings ++
-    updateOnDependencyChange ++
-    publishSettings
+      extraResolvers ++
+      extraLibraryDependencies ++
+      scalaSettings ++
+      updateOnDependencyChange ++
+      publishSettings
 
-  val projectName = "PersistentMap"
   lazy val root = {
-    val settings = moreSettings ++ Seq(name := projectName, fork := true)
+    val settings = moreSettings ++ Seq(fork := true)
     Project(id = projectName, base = file("."), settings = settings)
   }
 }

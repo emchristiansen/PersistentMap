f26b8bd094316a7507a106846ba1f6be15b9f707
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-09-30 18:33:33 -0700

    moved to st.sparse namespace

---
+++ src/main/scala/st/sparse/persistentmap/package.scala
@@ -0,0 +1,9 @@
+package st.sparse
+
+package object persistentmap {
+  /**
+   * This exception is raised when `PersistentMap` attempts to connect to a
+   * backing table with an incompatible type.
+   */
+  class TableTypeException(message: String) extends RuntimeException(message)
+}
---
+++ src/test/scala/st/sparse/persistentmap/testPersistentMap.scala
@@ -0,0 +1,129 @@
+package st.sparse.persistentmap
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.FunSuite
+import org.scalacheck.Gen
+import scala.pickling._
+import scala.pickling.binary._
+import scala.slick.session.Database
+import java.io.File
+
+case class MyKey(a: Int, b: String)
+case class MyValue(a: MyKey, b: Double)
+
+class Base(val y: Int)
+case class Derived(val x: Int) extends Base(x)
+
+@RunWith(classOf[JUnitRunner])
+class TestPersistentMap extends FunSuite with GeneratorDrivenPropertyChecks {
+  // Creates a MySQL database, backed by some random temporary file.
+  def createDatabase = {
+    val tempFile = File.createTempFile("testPersistentMapDB", "sqlite")
+    Database.forURL(s"jdbc:sqlite:$tempFile", driver = "org.sqlite.JDBC")
+  }
+
+  test("sample code") {
+    val database: scala.slick.session.Database = createDatabase
+    
+    // Create a `PersistentMap`.
+    val map = PersistentMap.create[Int, String]("myMap", database)
+    
+    // Add key-value pairs.
+    map += 1 -> "no"
+    map += 2 -> "boilerplate"
+    
+    // Retrieve values.
+    assert(map(1) == "no")
+    
+    // Delete key-value pairs.
+    map -= 2
+    
+    // And do anything else supported by `collection.mutable.Map`.
+  }
+  
+  test("test the core Map api with sample data") {
+    val database = createDatabase
+
+    val map = PersistentMap.create[MyKey, MyValue]("test", database)
+
+    val key1 = MyKey(1, "one")
+    val key2 = MyKey(2, "two")
+
+    val value1 = MyValue(key1, 1.0)
+    val value2 = MyValue(key2, 2.0)
+
+    val kv11 = key1 -> value1
+    val kv22 = key2 -> value2
+    val kv12 = key1 -> value2
+
+    assert(map.get(key1) == None)
+    assert(map.get(key2) == None)
+    assert(map.toSet == Set())
+
+    map += kv11
+
+    assert(map.get(key1) == Some(value1))
+    assert(map.get(key2) == None)
+    assert(map.toSet == Set(kv11))
+
+    map += kv22
+
+    assert(map.get(key1) == Some(value1))
+    assert(map.get(key2) == Some(value2))
+    assert(map.toSet == Set(kv11, kv22))
+
+    map -= key1
+
+    assert(map.get(key1) == None)
+    assert(map.get(key2) == Some(value2))
+    assert(map.toSet == Set(kv22))
+
+    map += kv12
+
+    assert(map.get(key1) == Some(value2))
+    assert(map.get(key2) == Some(value2))
+    assert(map.toSet == Set(kv12, kv22))
+
+    // Now let's make sure this map is really persistent.
+    val map2 = PersistentMap.connect[MyKey, MyValue]("test", database).get
+
+    assert(map == map2)
+  }
+
+  // We ensure we can't connect to a table with the wrong type of data.
+  test("basic table connection time type safety") {
+    val database = createDatabase
+
+    PersistentMap.create[Int, Double]("test", database)
+
+    // A `Double` is not a `String`, so this should fail to connect.
+    intercept[TableTypeException] {
+      PersistentMap.connect[Int, String]("test", database)
+    }
+
+    // Do a similar test with the key.
+    intercept[TableTypeException] {
+      PersistentMap.connect[String, Double]("test", database)
+    }
+
+    // For the sake of paranoia, make sure we can connect with the proper type.
+    PersistentMap.connect[Int, Double]("test", database)
+  }
+
+  // We extract a base class from a `PersistentMap` of a derived class.
+  test("table connection time type safety with subclasses") {
+    val database = createDatabase
+
+    val map1 = PersistentMap.create[Int, Derived]("test", database)
+    map1 += 42 -> Derived(10)
+
+    val map2 = PersistentMap.connect[Int, Derived]("test", database).get
+    assert(map1 == map2)
+    
+    // Retrieve the base type from a map with the derived type.
+    val map3 = PersistentMap.connect[Int, Base]("test", database).get
+    assert((map1(42): Base).y == map3(42).y)
+  }
+}
